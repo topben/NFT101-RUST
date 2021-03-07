55e870a1adb735853dd50b368a5bdfb6db262e8b
sheep3 <747033643@qq.com>
2021-03-07 21:00:36 +0800

    dev: 新增pallet-nft

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -40,6 +40,7 @@ pub use frame_support::{
 
 /// Import the template pallet.
 pub use pallet_template;
+pub use pallet_nft;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -265,6 +266,9 @@ impl pallet_sudo::Trait for Runtime {
 impl pallet_template::Trait for Runtime {
 	type Event = Event;
 }
+impl pallet_nft::Trait for Runtime {
+	type Event = Event;
+}
 
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
@@ -283,6 +287,7 @@ construct_runtime!(
 		Sudo: pallet_sudo::{Module, Call, Config<T>, Storage, Event<T>},
 		// Include the custom logic from the template pallet in the runtime.
 		TemplateModule: pallet_template::{Module, Call, Storage, Event<T>},
+		NftModule: pallet_nft::{Module, Call, Storage, Event<T>},
 	}
 );
 
